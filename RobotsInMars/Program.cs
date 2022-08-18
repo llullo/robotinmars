82d1c4f0b1944c27c56f462e537d27d693593cac
lluis hernan <lluis.hernan-garrigos@capgemini.com>
2022-08-18 21:58:03 +0200

    solution created

---
+++ RobotsInMars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobotsInMars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
